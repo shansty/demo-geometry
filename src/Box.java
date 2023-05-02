4465332a6993076cc2adaf13bc918aa9230bf8c3
Anastasiya Shirochina <shirochina16@gmail.com>
2023-05-02 17:19:44 +0300

    refactor box class add volume method

--- src/Box.java
+++ src/Box.java
@@ -2,4 +2,8 @@ public class Box {
     double width;
     double height;
     double depth;
+    void volume () {
+        System.out.print("Объем равен ");
+        System.out.println(width * height * depth);
+    }
 }
