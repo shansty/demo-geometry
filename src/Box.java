3b265bcaf1bc61b71bf9c7c9fda9b88336b4a622
Anastasiya Shirochina <shirochina16@gmail.com>
2023-05-02 17:42:18 +0300

    refactor box class add constructor with params

--- src/Box.java
+++ src/Box.java
@@ -2,11 +2,10 @@ public class Box {
     double width;
     double height;
     double depth;
-    Box() {
-        System.out.println("Конструирование объекта Box");
-        width = 10;
-        height = 10;
-        depth = 10;
+    Box(double w, double h, double d) {
+        width = w;
+        height = h;
+        depth = d;
     }
     double volume() {
         return width * height * depth;
