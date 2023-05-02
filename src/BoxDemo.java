bc1d3f5f4416d55e198fd239d39cd4d71fb6acfa
Anastasiya Shirochina <shirochina16@gmail.com>
2023-05-02 17:43:31 +0300

    refactor boxdemo class use constructor with params

--- src/BoxDemo.java
+++ src/BoxDemo.java
@@ -1,8 +1,8 @@
 public class BoxDemo {
     public static void main(String args[]) {
 
-        Box mybox1 = new Box();
-        Box mybox2 = new Box();
+        Box mybox1 = new Box(10, 20, 15);
+        Box mybox2 = new Box(3, 6, 9);
         double vol;
         vol = mybox1.volume();
         System.out.println("Объем равен " + vol);
