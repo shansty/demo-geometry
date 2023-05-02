114c6842e1398e0a5cee4eb73cb77fbe2ec5c209
Anastasiya Shirochina <shirochina16@gmail.com>
2023-05-02 17:34:19 +0300

    refactor boxdemo class use setdim method

--- src/BoxDemo.java
+++ src/BoxDemo.java
@@ -4,12 +4,8 @@ public class BoxDemo {
         Box mybox1 = new Box();
         Box mybox2 = new Box();
         double vol;
-        mybox1.width = 10;
-        mybox1.height = 20;
-        mybox1.depth = 15;
-        mybox2.width = 3;
-        mybox2.height = 6;
-        mybox2.depth = 9;
+        mybox1.setDim(10, 20, 15);
+        mybox2.setDim(3, 6, 9);
         vol = mybox1.volume();
         System.out.println("Объем равен " + vol);
         vol = mybox2.volume();
