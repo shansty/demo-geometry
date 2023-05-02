b6167b61142dcc297518e6de0b46b6150b47df11
Anastasiya Shirochina <shirochina16@gmail.com>
2023-05-02 17:21:04 +0300

    refactor boxdemo class use volume method

--- src/BoxDemo.java
+++ src/BoxDemo.java
@@ -3,16 +3,13 @@ public class BoxDemo {
 
         Box mybox1 = new Box();
         Box mybox2 = new Box();
-        double vol;
         mybox1.width = 10;
         mybox1.height = 20;
         mybox1.depth = 15;
         mybox2.width = 3;
         mybox2.height = 6;
         mybox2.depth = 9;
-        vol = mybox1.width * mybox1.height * mybox1.depth;
-        System.out.println("Объем равен " + vol);
-        vol = mybox2.width * mybox2.height * mybox2.depth;
-        System.out.println("Объем равен " + vol);
+        mybox1.volume();
+        mybox2.volume();
     }
 }
